9002e0a5f4ebcaa296563b76a42d9060def02b89
Rosa Orihuela <rosa.orihuela@abtasty.com>
2025-09-23 21:21:06 +0200

    Changes to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import getVersion from './utils/getVersion';
 
 export default function App() {
   const location = useLocation();
-  const slug = location.pathname === '/' ? 'home' : location.pathname.replace(/^\/+/, ''); 
+  const slug = location.pathname === '/' ? 'home' : location.pathname.replace(/^\/+/, '');
+
 
   const story = useStoryblok(slug, { version: getVersion() });
 
+
   useStoryblokBridge(story?.id, (updatedStory) => {
-    story.content = updatedStory.content;
+    if (updatedStory) {
+      story.content = updatedStory.content; 
+    }
   });
 
   if (!story?.content) return <div>Loading...</div>;
