e78d55524cdbdc2ce8b4a279ab4379e1fe453c47
Rosa Orihuela <rosa.orihuela@abtasty.com>
2025-09-24 17:53:52 +0200

    added links

--- src/storyblok/Links.jsx
+++ src/storyblok/Links.jsx
@@ -1,14 +1,17 @@
 import { storyblokEditable } from "@storyblok/react";
 
 export default function Links({ blok }) {
+  const href = blok.links?.cached_url || "#";
+
   return (
-    <div {...storyblokEditable(blok)} className="links">
-      <a 
-        href={blok.url?.url || "#"} 
-        target="_blank" 
-        rel="noopener noreferrer"
+    <div {...storyblokEditable(blok)} className="links-wrapper">
+      <a
+        href={href}
+        target={blok.links?.linktype === "url" ? "_blank" : "_self"}
+        rel={blok.links?.linktype === "url" ? "noopener noreferrer" : undefined}
+        className="fancy-link"
       >
-        {blok.name}
+        {blok.title || "Unwritten"}
       </a>
     </div>
   );
