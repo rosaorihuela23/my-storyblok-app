6335d5762149d5b58b7ed7581cc3f28d938bac05
Rosa Orihuela <rosa.orihuela@abtasty.com>
2025-09-24 18:09:09 +0200

    added links styling

--- src/storyblok/Teaser.jsx
+++ src/storyblok/Teaser.jsx
@@ -3,7 +3,7 @@ import { storyblokEditable } from '@storyblok/react';
 export default function Teaser({ blok }) {
 	return (
 		<div {...storyblokEditable(blok)} className="teaser">
-			<h2>{blok.headline}</h2>
+			<h2 className="teaser-headline">{blok.headline}</h2>
 		</div>
 	);
 }
